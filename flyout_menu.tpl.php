051ee7a18ee43b103ecceeb3119536e3551d5a4e
unknown <B@.(none)>
2012-01-05 16:59:33 -0800

    module files

---
+++ flyout_menu.tpl.php
@@ -0,0 +1,14 @@
+<?php
+//Theme flyout content with the $flyout object
+ 
+ //example usage below
+ ?>
+<div class="inner_flyout">
+
+
+
+ <?php print $flyout->body['und'][0]['value'];?>
+
+ 
+ 
+ </div>
